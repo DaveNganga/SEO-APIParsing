a96605f8d02191938f5d77cbbdaceb52e17f69f4
Dave Ng'ang'a <dn789@cairn.edu>
2021-06-29 20:02:00 +0000

    a sample Json parsor

---
+++ SampleJsonParser.py
@@ -0,0 +1,17 @@
+# sample requests file and json parsor
+import requests
+import json
+
+file = requests.get("http://api.open-notify.org/astros.json")
+
+json_file = file.json()
+
+print("Status Code: ", file.status_code)
+
+counter = 0
+for Key in json_file['people']:
+  if counter == 5:
+    break
+  print(Key['name'])
+  counter += 1
+#print(file.json())
