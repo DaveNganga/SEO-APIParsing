85cebcb42874084ba03e9b5c1a230dc5b05098e4
Dave Ng'ang'a <dn789@cairn.edu>
2021-07-01 13:17:19 +0000

    spotipy

--- apis.py
+++ apis.py
@@ -1,5 +1,6 @@
 import requests
 import spotipy
+import json 
 
 CLIENT_ID = "c4820cbbfb4b4bf68e69962fd1648da4"
 CLIENT_SECRET = "968c1ca80cc148e9b29310afab75300e"
@@ -13,3 +14,18 @@ auth_response = requests.post(AUTH_URL, {
 })
 print(auth_response.status_code)
 auth_response_data = auth_response.json()
+access_token = auth_response_data['access_token']
+
+#Try and get information on a specific song: Sacrifices - dreamville
+
+track_id = '7wTA0NKIm6T7nP2kaymU2a?si=55326fdadbb74623'
+track_Id = '844fefa17b9f438a'
+BASE_URL = 'https://api.spotify.com/v1/'
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+r = r.json()
+print(r)
+
