7227c42bb23a71b79ab0329fb7af24c58ef89870
Dave Ng'ang'a <dn789@cairn.edu>
2021-07-05 18:36:07 +0000

    further editing

--- apis.py
+++ apis.py
@@ -1,6 +1,8 @@
 import requests
 import spotipy
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import sqlalchemy
 from sqlalchemy import create_engine
 
@@ -31,32 +33,50 @@ def response_to_accesstoken(response):
 
 # Function get information on a specific song: Sacrifices - dreamville
 def track_information(access_token):
-    track_id = '7wTA0NKIm6T7nP2kaymU2a?si=55326fdadbb74623'
+    track_id = '7wTA0NKIm6T7nP2kaymU2a' # Sacrifices - dreamville
+    track_id_two = '0u7qg774VdGANjp5f4uRNv' # Niko Sawa - Sauti Sol
+    track_id_three = '2Rh2RQOFkjqdyIFVzgdmfH' # Festival minor - Gerry M Sextet
     BASE_URL = 'https://api.spotify.com/v1/'
     headers = {
         'Authorization': 'Bearer {token}'.format(token=access_token)
     }
     r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+    r2 = requests.get(BASE_URL + 'audio-features/' + track_id_two, headers=headers)
+    r3 = requests.get(BASE_URL + 'audio-features/' + track_id_three, headers=headers)
+    
     data = r.json()
-    print('The raw data: ', data)
-    return data
+    data2 = r2.json()
+    data3 = r3.json()
+    for column in ['uri','track_href','analysis_url']:
+        data.pop(column)
+        data2.pop(column)
+        data3.pop(column)
+
+    print('The raw data: ', data, data2, data3)
+    return [data, data2, data3]
 
 
 # Function to insert this ifomration into a local sql database
 def data_to_sql(data):
-    information_data_frame = pd.DataFrame.from_dict([data], orient='columns')
-    print(information_data_frame)
-    engine = create_engine('mysql://root:codio@localhost/music')
-    information_data_frame.to_sql('SongInformation', con=engine,
-                                  if_exists='replace', index=False)
-
-
-def main():
-    # A main function to call all the units together
+    for data_object in data:
+        information_data_frame = pd.DataFrame.from_dict([data_object], orient='columns')
+        print(information_data_frame)
+        engine = create_engine('mysql://root:codio@localhost/music')
+        information_data_frame.to_sql('SongInformation', con=engine,
+                                      if_exists='append', index=False)
+        
+def data_to_visualization():
+    ypoints = np.array([3,4,5,6,-20,2,41,])
+    
+    plt.plot(ypoints, linestyle = 'dotted')
+    plt.show()  
+  
+  
+    # Main function to call all the units together
+if __name__ == '__main__':
     Auth_Response = spotipy_client()
     Access_Token = response_to_accesstoken(Auth_Response)
     Track_Information = track_information(Access_Token)
     data_to_sql(Track_Information)
+    data_to_visualization()
 
-    if __name__ == '__main__':
-        main()
